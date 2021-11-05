516382cd48b3f548cb2647ce85af1f8347211e06
German Zvezdin <germanzvezdin@yandex.ru>
2021-11-05 19:58:24 +0300

    feat: added code generator for cuda

--- generator/main.cpp
+++ generator/main.cpp
@@ -5,22 +5,12 @@
 //  Created by German Zvezdin on 30.10.2021.
 //
 
-#include <iostream>
-#include "polygon.hpp"
+#include "utils.hpp"
 
 int main() {
-    icosahedron i(1);
-    auto w = generator(i);
-    std::cout << i << std::endl;
-    std::cout << "device const ftype wiq[] = {" << std::endl;
-    std::cout << w[0] << "," << std::endl;
-    for(int i = 0; i < 5; i++){
-        std::cout << w[1] << "," << std::endl;
-    }
-    for(int i = 0; i < 5; i++){
-        std::cout << w[2] << "," << std::endl;
-    }
-    std::cout << w[3] << "};" << std::endl;
-    std::cout << "const ftype w0 = " << w[4] << ";" <<std::endl;
+    double r = sqrt(3.0 / 2.0);
+    //D3Q15(r);
+    //D3Q27();
+    D3Q39(r);
     return 0;
 }
