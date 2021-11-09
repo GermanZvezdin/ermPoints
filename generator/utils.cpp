4811467c319f2682b1c327fc851f8f2280cde523
German Zvezdin <germanzvezdin@yandex.ru>
2021-11-09 13:37:34 +0300

    fix: code style for cuda is fixed

--- generator/utils.cpp
+++ generator/utils.cpp
@@ -14,7 +14,7 @@ void D3Q39(double r){
     auto w = D3Q39Generator(o1, c, o2, cc, o3);
     printf("#define DIM 3\n");
     printf("const int Nq = 38, NQ = Nq+1;\n");
-    printf("device const ftype3 ciq[] = \n");
+    printf("__device__ const ftype3 ciq[] = \n");
     printf("{{0.0, 0.0, 0.0},\n");
     for(int i = 0; i < 6; i++) {
         printf("{%1.15lf, %1.15lf, %1.15lf},\n",o1.get().row(i)[0], o1.get().row(i)[1], o1.get().row(i)[2]);
@@ -35,7 +35,7 @@ void D3Q39(double r){
     for(int i = 0; i < w.size(); i++) {
         printf("double w%d = %1.15lf;\n",i, w[i]);
     }
-    printf("device const ftype wiq[] = {w0, ");
+    printf("__device__ const ftype wiq[] = {w0, ");
     for(int i = 0; i < 6; i++) {
         printf("w1, ");
     }
@@ -60,7 +60,7 @@ void D3Q27(double r, double s, double t) {
     auto w = D3Q27Generator(c, o, cc);
     printf("#define DIM 3\n");
     printf("const int Nq = 26, NQ = Nq+1;\n");
-    printf("device const ftype3 ciq[] = \n");
+    printf("__device__ const ftype3 ciq[] = \n");
     printf("{{0.0, 0.0, 0.0},\n");
     for(int i = 0; i < 6; i++) {
         printf("{%1.15lf, %1.15lf, %1.15lf},\n",o.get().row(i)[0], o.get().row(i)[1], o.get().row(i)[2]);
@@ -78,7 +78,7 @@ void D3Q27(double r, double s, double t) {
     printf("double w2 = %1.15lf;\n",w[2]); // cc
     printf("double w3 = %1.15lf;\n",w[0]); // c
     
-    printf("device const ftype wiq[] = {w0, ");
+    printf("__device__ const ftype wiq[] = {w0, ");
     for(int i = 0; i < 6; i++) {
         printf("w1, ");
     }
@@ -98,7 +98,7 @@ void D3Q15(double r) {
     auto w = D3Q15Generator(c, o);
     printf("#define DIM 3\n");
     printf("const int Nq = 26, NQ = Nq+1;\n");
-    printf("device const ftype3 ciq[] = \n");
+    printf("__device__ const ftype3 ciq[] = \n");
     printf("{{0.0, 0.0, 0.0},\n");
     for(int i = 0; i < 6; i++) {
         printf("{%1.15lf, %1.15lf, %1.15lf},\n", o.get().row(i)[0], o.get().row(i)[1], o.get().row(i)[2]);
@@ -112,7 +112,7 @@ void D3Q15(double r) {
     printf("double w1 = %1.15lf;\n",w[0]); // oct
     printf("double w2 = %1.15lf;\n",w[1]); // c
     
-    printf("device const ftype wiq[] = {w0, ");
+    printf("__device__ const ftype wiq[] = {w0, ");
     for(int i = 0; i < 6; i++) {
         printf("w1, ");
     }
